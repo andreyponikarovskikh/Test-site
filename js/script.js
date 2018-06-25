2f5c35b9e76b75499ed6370d4a7bd151146d0b7f
Andrey Ponikarovskikh <a.ponikarovskikh@gmail.com>
2018-06-25 11:02:31 +0500

    plus pop-up

---
+++ js/script.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var popupForm = document.querySelector(".popUpForm");
+var popupButton = document.querySelector(".js_popUpButton");
+var formButton = document.querySelector(".js_sendFormButton")  ;
+var clientName = popupForm.querySelector("[name=clientName]");
+
+popupButton.addEventListener("click", function (evt) {
+evt.preventDefault();
+popupForm.classList.toggle("popUpForm__popUp");
+// clientName.focus();  на экране творится какая-то ерунда ?????
+});
+
+formButton.addEventListener("click", function (evt) {
+evt.preventDefault();
+popupForm.classList.toggle("popUpForm__popUp");
+});
+
+
+window.addEventListener("keydown", function (evt) {
+if (evt.keyCode === 27) {
+  evt.preventDefault();
+  if (popupForm.classList.contains("popUpForm__popUp")) {
+    popupForm.classList.remove("popUpForm__popUp");
+  }
+}
+});
